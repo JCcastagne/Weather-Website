155d9b7c1002c23f0aeaeda7d2decad84437ac68
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2020-12-30 18:32:38 -0500

    Fix image urls for GitHub Pages.

--- js/getLocation.js
+++ js/getLocation.js
@@ -146,15 +146,15 @@ function buildData(data) {
     <h3>Stats</h3>
     <div class="container">
 
-    <p><img src="./../img/icons/pressure.svg" alt="pressure icon">
+    <p><img src="./img/icons/pressure.svg" alt="pressure icon">
     Pressure ${data.current.pressure}mb</p>
-    <p><img src="./../img/icons/humidity.svg" alt="humidity icon">
+    <p><img src="./img/icons/humidity.svg" alt="humidity icon">
     Humidity ${data.current.humidity}&#37;</p>
-    <p><img src="./../img/icons/dew.svg" alt="dew point icon">
+    <p><img src="./img/icons/dew.svg" alt="dew point icon">
     Dew ${dewPoint}&#176;C</p>
-    <p><img src="./../img/icons/uvi.svg" alt="uvi icon">
+    <p><img src="./img/icons/uvi.svg" alt="uvi icon">
     Uvi ${data.current.uvi}</p>
-    <p><img src="./../img/icons/visibility.svg" alt="visibility icon">
+    <p><img src="./img/icons/visibility.svg" alt="visibility icon">
     Visibility ${data.current.visibility} Ft.</p>
 
     </div>
@@ -186,7 +186,7 @@ function buildData(data) {
       hourlyHTML = hourlyHTML.concat(`
       <div class="container">
         <h4>${hourlyTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})}</h4>
-        <img src="/img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
+        <img src="./img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
         <span>${data.hourly[i]['weather'][0]['description']}</span>
 
         <div class="graph">
@@ -208,7 +208,7 @@ function buildData(data) {
       hourlyHTML = hourlyHTML.concat(`
       <div class="container">
         <h4>${hourlyTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})}</h4>
-        <img src="/img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
+        <img src="./img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
         <span>${data.hourly[i]['weather'][0]['description']}</span>
 
         <div class="graph">
@@ -230,7 +230,7 @@ function buildData(data) {
       hourlyHTML = hourlyHTML.concat(`
       <div class="container">
         <h4>${hourlyTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})}</h4>
-        <img src="/img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
+        <img src="./img/weatherIcons/SVG/${data.hourly[i]['weather']['0']['icon']}.svg" alt="weather icon for hour ${hourlyTime}">
         <span>${data.hourly[i]['weather'][0]['description']}</span>
 
         <div class="graph">
